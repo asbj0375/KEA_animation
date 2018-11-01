da850ee347be7deeafc725c7bdc65bafbddc1b02
Asbjørn Elle Schmidt <godbear@laptop-edlij0a3.kea.local>
2018-11-01 13:21:22 +0100

    point og liv, med fejl

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function sidenVises() {
 function showStart() {
     console.log("showStart");
     // Vis startskærm
-    document.querySelector("#start").classList.add("show");
+    document.querySelector("#start").classList.remove("hide");
     // Start animation på start-knap
     document.querySelector("#play").classList.add("pulse");
     // Start animation på settings-knap
@@ -23,14 +23,13 @@ function showStart() {
     document.querySelector("#broccoli_bat").classList.add("bat");
     document.querySelector("#gulerod_smoke").classList.add("smoke");
 
-
-
-    //Når der er klikket på start knap gå til hideStart
-
-    document.querySelector("#play").addEventListener("click", hideStart);
-
-
 }
+
+//Når der er klikket på start knap gå til hideStart
+
+document.querySelector("#play").addEventListener("click", hideStart);
+
+
 
 function hideStart() {
     console.log("hideStart");
@@ -49,6 +48,77 @@ function hideStart() {
 function startGame() {
     console.log("startGame");
     // Skjul startskærm
-    document.querySelector("#start").classList.add("hide");
-    // Vis spilskærm
+
+    document.querySelector("#frikadelle").addEventListener("click", clickDunse);
+    document.querySelector("#broccoli").addEventListener("click", clickBroccoli);
+    document.querySelector("#gulerod").addEventListener("click", clickGulerod);
 }
+
+
+let point = 0;
+let liv = 3;
+
+function clickDunse() {
+    console.log("click dunse");
+    point++;
+    console.log(point);
+    document.querySelector(".antal").textContent = point;
+}
+
+
+function clickBroccoli() {
+    console.log("click broccoli");
+    liv--;
+    console.log(liv);
+    let heart = "#heart" + liv;
+    document.querySelector(heart).classList.add("fade_out");
+}
+
+
+
+function clickGulerod() {
+    console.log("click gulerod");
+    point--;
+    console.log(point);
+    document.querySelector(".antal").textContent = point;
+}
+
+
+
+
+//let point = 0;
+//
+//
+//function clickDunse() {
+//    console.log("click dunse");
+//
+//    // TODO: giv point!
+//
+//    point++;
+//    console.log(point);
+//
+//    document.querySelector(".antal").textContent = point;
+//
+//    // også TODO: Få det til at virke så mønten starter forfra - det må vente
+//}
+//
+//
+//function clickGulerod() {
+//    console.log("click gulerod");
+//}
+//
+//let liv = 3;
+//
+//function clickBroccoli() {
+//    console.log("click broccoli");
+//
+//    // TODO: mist et liv
+//    let heart = "#heart3";
+//    document.querySelector(heart).classList.add("dissappear");
+//    console.log("#heart" + liv);
+//    liv--;
+//    console.log(liv);
+//
+//
+//    // også TODO: Få eksplosionen til at virke igen - det må også vente
+//}
